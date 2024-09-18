b46841229887ef468e88524362e87c8ac16d0f06
Agustinus Theodorus <agustinustheoo@gmail.com>
2024-09-18 23:32:20 +0700

    feat: add mongorestore installation

--- linux/scripts/setup_mongodb.sh
+++ linux/scripts/setup_mongodb.sh
@@ -7,6 +7,27 @@ if [ ! -f myriad-setup/docker-compose.yml ]; then
     exit 1
 fi
 
+# Check if mongorestore is already installed
+if ! command -v mongorestore &> /dev/null; then
+    echo "mongorestore not found. Downloading and installing MongoDB database tools..."
+    
+    # Download and install MongoDB database tools
+    wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2204-x86_64-100.10.0.deb
+    
+    echo "Installing MongoDB database tools..."
+    sudo dpkg -i mongodb-database-tools-ubuntu2204-x86_64-100.10.0.deb
+    
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to install MongoDB database tools. Please check the installation output and try again."
+        exit 1
+    fi
+    
+    # Clean up the downloaded .deb file
+    rm mongodb-database-tools-ubuntu2204-x86_64-100.10.0.deb
+else
+    echo "mongorestore is already installed. Proceeding with MongoDB setup."
+fi
+
 # Read MongoDB credentials from docker-compose.yml
 mongo_user=$(grep MONGO_INITDB_ROOT_USERNAME myriad-setup/docker-compose.yml | awk '{print $2}')
 mongo_password=$(grep MONGO_INITDB_ROOT_PASSWORD myriad-setup/docker-compose.yml | awk '{print $2}')
@@ -22,20 +43,20 @@ mkdir -p mongodb_data
 docker compose -f myriad-setup/docker-compose.yml up -d mongodb
 
 echo "Waiting for MongoDB to start..."
-sleep 10
+sleep 20
 
 # Construct MongoDB URI
 URI="mongodb://$mongo_user:$mongo_password@localhost:$mongo_port/myriad?authSource=admin"
 
 # Check if dump directory exists
-if [ ! -d "dump" ]; then
-    echo "Error: 'dump' directory not found. Please ensure it exists in the current directory."
+if [ ! -d "myriad-setup/mongodb/dump" ]; then
+    echo "Error: 'myriad-setup/mongodb/dump' directory not found. Please ensure it exists."
     exit 1
 fi
 
 # Restore data from dump
 echo "Restoring data to MongoDB..."
-mongorestore --uri="$URI" dump/
+mongorestore --uri="$URI" myriad-setup/mongodb/dump/
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to restore data to MongoDB. Please check the mongorestore output and try again."
