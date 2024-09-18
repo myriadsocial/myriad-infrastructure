2cc6896b8971bb774eb27a3572139741e1a47aeb
Agustinus Theodorus <agustinustheoo@gmail.com>
2024-09-19 00:39:06 +0700

    feat: generate jwt and app secrets in download_files.sh

--- linux/scripts/download_files.sh
+++ linux/scripts/download_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# download_files.sh - Script to download required files
+# download_files.sh - Script to download required files and generate random credentials
 
 mkdir -p myriad-setup
 cd myriad-setup
@@ -13,6 +13,11 @@ mongo_password=$(openssl rand -hex 16)
 minio_root_user=$(openssl rand -hex 8)
 minio_root_password=$(openssl rand -hex 16)
 
+# Generate new parameters
+app_secret=$(openssl rand -hex 32)
+jwt_token_secret_key=$(openssl rand -hex 32)
+jwt_refresh_token_secret_key=$(openssl rand -hex 32)
+
 # Update docker-compose.yml with random credentials
 sed -i "s/MONGO_INITDB_ROOT_USERNAME: .*/MONGO_INITDB_ROOT_USERNAME: $mongo_user/" docker-compose.yml
 sed -i "s/MONGO_INITDB_ROOT_PASSWORD: .*/MONGO_INITDB_ROOT_PASSWORD: $mongo_password/" docker-compose.yml
@@ -24,6 +29,11 @@ sed -i "s/MONGO_USER=.*/MONGO_USER=$mongo_user/" docker-compose.yml
 sed -i "s/MONGO_PASSWORD=.*/MONGO_PASSWORD=$mongo_password/" docker-compose.yml
 sed -i "s|MONGO_URL=.*|MONGO_URL=mongodb://$mongo_user:$mongo_password@myriad-mongo:27017/myriad?authSource=admin|" docker-compose.yml
 
+# Update new parameters
+sed -i "s/APP_SECRET=.*/APP_SECRET=$app_secret/" docker-compose.yml
+sed -i "s/JWT_TOKEN_SECRET_KEY=.*/JWT_TOKEN_SECRET_KEY=$jwt_token_secret_key/" docker-compose.yml
+sed -i "s/JWT_REFRESH_TOKEN_SECRET_KEY=.*/JWT_REFRESH_TOKEN_SECRET_KEY=$jwt_refresh_token_secret_key/" docker-compose.yml
+
 mkdir -p mongodb/dump/myriad
 cd mongodb/dump/myriad
 wget --no-cache https://raw.githubusercontent.com/myriadsocial/myriad-infrastructure/main/linux/mongodb/dump/myriad/currencies.bson
@@ -33,3 +43,13 @@ wget --no-cache https://raw.githubusercontent.com/myriadsocial/myriad-infrastruc
 wget --no-cache https://raw.githubusercontent.com/myriadsocial/myriad-infrastructure/main/linux/mongodb/dump/myriad/servers.bson
 wget --no-cache https://raw.githubusercontent.com/myriadsocial/myriad-infrastructure/main/linux/mongodb/dump/myriad/servers.metadata.json
 cd ../../..
+
+echo "Files downloaded and credentials updated successfully."
+echo "MongoDB user: $mongo_user"
+echo "MongoDB password: $mongo_password"
+echo "MinIO root user: $minio_root_user"
+echo "MinIO root password: $minio_root_password"
+echo "APP_SECRET: $app_secret"
+echo "JWT_TOKEN_SECRET_KEY: $jwt_token_secret_key"
+echo "JWT_REFRESH_TOKEN_SECRET_KEY: $jwt_refresh_token_secret_key"
+echo "Please make sure to update any other necessary configuration files with these new credentials and parameters."
